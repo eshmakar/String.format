acb520f6e208df56abde5a36b4030eab4335f515
Marat <eshmakar@mail.ru>
2020-05-21 02:21:57 +0300

    небольшие изменения

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,6 +7,7 @@
 %%	Символ %*/
 public class Test {
     public static void main(String[] args) {
+        String s = "hello";
         int d = 15;
         double f = 15.55;
         boolean b = true;
@@ -15,17 +16,19 @@ public class Test {
 
 
         // -30 после знака процента означает, сколько пробелов добавлять после значение
-        // +30 после знака процента означает, сколько пробелов добавлять перед значением
-        System.out.printf("This is %+30d int, long\n", d);
-        System.out.printf("This is %+30f double, float\n", f);
+        // 30 после знака процента означает, сколько пробелов добавлять перед значением
+        System.out.printf("This is %30s String\n", s);
+        System.out.printf("This is %30d int, long\n", d);
+        System.out.printf("This is %30f double, float\n", f);
         System.out.printf("This is %-30b boolean\n", b);
         System.out.printf("This is %-30c char\n", c);
         System.out.printf("This is %-30% percent format\n", percent);
 
         /**OUTPUT
 
-         This is                            +15 int, long
-         This is                     +15,550000 double, float
+         This is                          hello String
+         This is                             15 int, long
+         This is                      15,550000 double, float
          This is true                           boolean
          This is C                              char
          This is %                              percent format
